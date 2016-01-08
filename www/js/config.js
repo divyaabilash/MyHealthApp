0f6c76602f4cd97efef60ed5dc7030f7f87d3442
Divya Abilash <divya@Abilashs-MacBook-Pro.local>
2016-01-08 11:17:31 -0800

    Addition of time label in prescription list and key word label in contact list

--- www/js/config.js
+++ www/js/config.js
@@ -1,3 +1,3 @@
 angular.module('app.config', [])
-//.constant('baseUrl', 'http://localhost:5000');
-    .constant('baseUrl', 'https://dry-wildwood-8096.herokuapp.com');
+// .constant('baseUrl', 'http://localhost:5000');
+   .constant('baseUrl', 'https://dry-wildwood-8096.herokuapp.com');
